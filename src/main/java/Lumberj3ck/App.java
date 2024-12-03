56c564044d45f8cb6d46754515338a5b514e56d6
unknown <alanuldashev@gmail.com>
2024-12-03 20:15:54 +0100

    data building getting data from specific starting data alpaca api

--- src/main/java/Lumberj3ck/App.java
+++ src/main/java/Lumberj3ck/App.java
@@ -1,12 +1,14 @@
 package Lumberj3ck;
 
+import java.time.LocalDate;
 
 
 public class App{
     public static void main(String[] args) {
         MarketDataProvider provider = new MarketDataProvider();
         provider.getDataFromMarket("AAPL");
-        provider.getClosingPrices(, 10);
+        LocalDate startingDate = LocalDate.of(2024, 10, 1);
+        provider.getClosingPrices(startingDate, "1D");
         provider.closeClient();
     }
 }
