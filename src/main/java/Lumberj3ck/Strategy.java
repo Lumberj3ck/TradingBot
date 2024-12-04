246c122844275b5f7e295068a020e6bf2164bc6b
timurenk0 <zheltenkovtimur@gmail.com>
2024-12-04 14:32:45 +0100

    Changed Strategy class to Abstract and added abstract methods

--- src/main/java/Lumberj3ck/Strategy.java
+++ src/main/java/Lumberj3ck/Strategy.java
@@ -1,16 +1,12 @@
 package Lumberj3ck;
 
-public class Strategy {
-   
-    Strategy(){
+abstract public class Strategy {
 
-    } 
+    abstract public void buy();
 
-    public void buy(){
+    abstract public void sell();
 
-    }
+    abstract public boolean shouldEnterMarket();
 
-    public void sell(){
-
-    }
+    abstract public boolean shouldExitMarket();
 }
