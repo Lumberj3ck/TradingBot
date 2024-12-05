5be17c90ea11b40e1f7981ea8b6f72470bf8e64c
timurenk0 <zheltenkovtimur@gmail.com>
2024-12-05 17:06:15 +0100

    Added alpaca dependency in pom.xml file

--- src/main/java/Lumberj3ck/TradeExecutor.java
+++ src/main/java/Lumberj3ck/TradeExecutor.java
@@ -2,11 +2,11 @@ package Lumberj3ck;
 
 abstract class TradeExecutor {
     // not must method maybe overwroted
-    public void buy(String symbol, int amount){
+    public void buy(String symbol, int amount) {
         // buying logic
     }
 
-    public void sell(String symbol){
-        // selling logic 
+    public void sell(String symbol) {
+        // selling logic
     };
 }
