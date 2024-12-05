c89a35b698f3c4e30a236598bd5c24612f7544f9
unknown <alanuldashev@gmail.com>
2024-12-05 01:50:56 +0100

    Runner functionality

---
+++ src/main/java/Lumberj3ck/Runner.java
@@ -0,0 +1,33 @@
+package Lumberj3ck;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Runner {
+
+    public static void run(Strategy strategy){
+        ArrayList<String> symbols = new ArrayList<>(Arrays.asList("AAPL", "GOOGL", "MSFT"));
+
+        while (true){
+            for (String symbol : symbols){
+                strategy.shouldEnterMarket(symbol);
+                if (!strategy.isPositionOpen(symbol) && strategy.shouldEnterMarket(symbol)){
+                    // for now hardcoded
+                    int amount = 5;
+                    strategy.buy(symbol, amount);
+                } else if (strategy.isPositionOpen(symbol) && strategy.shouldExitMarket(symbol)){
+                    strategy.sell(symbol);
+                }
+            }
+            //  for now just sleep for hour
+            int second = 1000;
+            int hour = second * 3600;
+            try {
+                Thread.sleep(hour);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+    }
+}
