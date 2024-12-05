1269f51239c0fd6371a3a988c7ee1706cb737f19
unknown <alanuldashev@gmail.com>
2024-12-05 02:16:26 +0100

    TradeExecutor abstraction added

---
+++ src/main/java/Lumberj3ck/TradeExecutor.java
@@ -0,0 +1,12 @@
+package Lumberj3ck;
+
+abstract class TradeExecutor {
+    // not must method maybe overwroted
+    public void buy(String symbol, int amount){
+        // buying logic
+    }
+
+    public void sell(String symbol){
+        // selling logic 
+    };
+}
