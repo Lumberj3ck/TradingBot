f1b95eb5f0a1e1f890a4a1ffa364dee8d6bfccf4
unknown <alanuldashev@gmail.com>
2024-12-06 14:58:47 +0100

    decoupled TradeExecutor from Abstract Strategy

--- src/main/java/Lumberj3ck/Strategy.java
+++ src/main/java/Lumberj3ck/Strategy.java
@@ -1,25 +1,12 @@
 package Lumberj3ck;
 
-import net.jacobpeterson.alpaca.openapi.trader.ApiException;
 
 abstract public class Strategy {
-    protected TradeExecutor executor;
+    // protected TradeExecutor executor;
 
-    public Strategy(TradeExecutor executor) {
-        this.executor = executor;
-    }
-
-    public void buy(String symbol, String amount) throws ApiException, InterruptedException {
-        // selling logic
-        // just calling executor
-        this.executor.buy(symbol, amount);
-    }
-
-    public void sell(String symbol, String amount) {
-        // selling logic
-        // just calling executor
-        this.executor.sell(symbol, amount);
-    };
+    // public Strategy(TradeExecutor executor) {
+    //     this.executor = executor;
+    // }
 
     public boolean isPositionOpen(String symbol) {
         // checking logic
