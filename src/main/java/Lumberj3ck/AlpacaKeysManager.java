0b25e4a5bf90d2f448c164b1ad73ca508b85ddf6
unknown <alanuldashev@gmail.com>
2024-12-08 17:39:21 +0100

    okhttp default headers for authorisation implemented

--- src/main/java/Lumberj3ck/AlpacaKeysManager.java
+++ src/main/java/Lumberj3ck/AlpacaKeysManager.java
@@ -1,17 +1,31 @@
 package Lumberj3ck;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 public class AlpacaKeysManager {
-    public final String api_key;
-    public final String api_secret_key;
-    public final String url;
+    private final String api_key;
+    private final String api_secret_key;
+    public OkHttpClient client;
 
     AlpacaKeysManager(){
         Dotenv de = Dotenv.load();
 
         this.api_key = de.get("api_key");
         this.api_secret_key = de.get("api_secret_key");
-        this.url = "https://paper-api.alpaca.markets/v2/orders";
+
+        this.client = new OkHttpClient.Builder()
+        .addInterceptor(chain -> {
+            Request originalRequest = chain.request();
+            Request newRequest = originalRequest.newBuilder()
+                .addHeader("APCA-API-KEY-ID", this.api_key)
+                .addHeader("APCA-API-SECRET-KEY", this.api_secret_key)
+                .addHeader("accept", "application/json")
+                .build();
+            return chain.proceed(newRequest);
+        })
+        .build();
+
     }
 }
