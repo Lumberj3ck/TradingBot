0c6f83a6b9ad4b0b44fc566b5ba725b2233da4c0
unknown <alanuldashev@gmail.com>
2024-12-10 12:17:24 +0100

    added timeseries method builder | added symbol to closing price

--- src/main/java/Lumberj3ck/MarketDataProvider.java
+++ src/main/java/Lumberj3ck/MarketDataProvider.java
@@ -1,10 +1,18 @@
 package Lumberj3ck;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.ta4j.core.Bar;
+import org.ta4j.core.BaseBar;
+import org.ta4j.core.num.DecimalNum;
 
 import okhttp3.Request;
 import okhttp3.Response;
@@ -15,7 +23,7 @@ public class MarketDataProvider {
     MarketDataProvider() {
         this.manager = new AlpacaKeysManager();
     }
-    
+
     public void getDataFromMarket(String symbol) {
         String requestUrl = String.format(
                 "https://data.alpaca.markets/v2/stocks/trades?symbols=%s&start=2024-09-01&limit=1000&feed=sip&sort=asc",
@@ -38,9 +46,10 @@ public class MarketDataProvider {
         }
     }
 
-    public ArrayList<Integer> getClosingPrices(LocalDate start, String timeframe) {
+    public ArrayList<Bar> getClosingPrices(String symbol, LocalDate start, String timeframe) {
         String requestUrl = String.format(
-                "https://data.alpaca.markets/v2/stocks/bars?symbols=AAPL&timeframe=%s&start=%s&limit=1000&adjustment=raw&feed=sip&sort=asc",
+                "https://data.alpaca.markets/v2/stocks/bars?symbols=%s&timeframe=%s&start=%s&adjustment=raw&feed=sip&sort=asc",
+                symbol,
                 timeframe, start);
         System.out.println(requestUrl);
         Request request = new Request.Builder()
@@ -52,16 +61,35 @@ public class MarketDataProvider {
             String jsonData = response.body().string();
             JSONObject Jobject = new JSONObject(jsonData);
 
-            System.out.println(Jobject.optString("next_page_toke"));
-            // symbol hardcoded for now
-            return buildData(Jobject, "AAPL");
+            return createTimeSeriesFromAlpacaResponse(Jobject, symbol);
         } catch (Exception e) {
             System.out.println(e);
         }
         return new ArrayList<>();
     }
 
-    public ArrayList<Integer> buildData(JSONObject jsonData, String symbol){
+    private ArrayList<Bar> createTimeSeriesFromAlpacaResponse(JSONObject jsonData, String symbol) {
+        JSONObject stock_bars = jsonData.getJSONObject("bars");
+        JSONArray bars = stock_bars.getJSONArray(symbol);
+        ArrayList<Bar> seriesBars = new ArrayList<>();
+
+        for (Object barObj : bars) {
+            JSONObject bar = (JSONObject) barObj;
+            Bar newBar = new BaseBar(Duration.ofDays(1),
+                    ZonedDateTime.ofInstant(Instant.parse("2024-04-05T04:00:00Z"), ZoneId.systemDefault()),
+                    // bar.getString("t"),
+                    bar.getDouble("o"),
+                    bar.getDouble("h"),
+                    bar.getDouble("l"),
+                    bar.getDouble("c"),
+                    bar.getLong("v"));
+            seriesBars.add(newBar);
+        }
+
+        return seriesBars;
+    }
+
+    public ArrayList<Integer> buildData(JSONObject jsonData, String symbol) {
         JSONObject bars = jsonData.getJSONObject("bars");
         JSONArray prices = bars.getJSONArray(symbol);
         ArrayList<Integer> finalPrices = new ArrayList<>();
@@ -72,17 +100,8 @@ public class MarketDataProvider {
         return finalPrices;
     }
 
-    public static double calculateSMA(ArrayList<Integer> closingPrices){
-        int priceSum = 0;
-        for (int cp : closingPrices){
-            priceSum += cp;
-        }
-        return priceSum / closingPrices.size();
-    }
-
     public void closeClient() {
         this.manager.client.connectionPool().evictAll();
         this.manager.client.dispatcher().executorService().shutdown();
     }
 }
-
