cb07f547c8843a467f2e8c63632a3edaa1a1e5fb
unknown <alanuldashev@gmail.com>
2024-12-10 14:12:34 +0100

    sleeping after succesfull stock checking cycle

--- src/main/java/Lumberj3ck/Runner.java
+++ src/main/java/Lumberj3ck/Runner.java
@@ -5,9 +5,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
 
-// import java.util.ArrayList;
-
-// import java.util.Arrays;
 
 public class Runner {
     private static void stockCheckCycle(Strategy strategy, TradeExecutor executor) {
@@ -25,38 +22,38 @@ public class Runner {
                 System.out.println(String.format("We are already bought %s, but don't wanna sell !!", symbol));
             }
         }
-
-        // for now just sleep for hour
-        int second = 1000;
-        // int hour = second * 3600;
-        try {
-            Thread.sleep(second * 5);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
     }
 
     public static void run(Strategy strategy, TradeExecutor executor) {
         MarketDataProvider mp = new MarketDataProvider();
         while (true) {
-            Map<String, Object> result  = mp.isMarketOpen();
+            Map<String, Object> result = mp.isMarketOpen();
             Boolean isOpen = (Boolean) result.get("is_open");
             Duration duration = (Duration) result.get("leftToNext");
 
             long hours = duration.toHours();
             long minutes = duration.toMinutesPart();
-            String openClosed = isOpen ? "open" : "closed"; 
+            String openClosed = isOpen ? "open" : "closed";
 
-            System.out.println("Market is" + openClosed); 
+            System.out.println("Market is" + openClosed);
             if (isOpen) {
                 stockCheckCycle(strategy, executor);
             } else {
-                System.out.println("Market will be open in " + duration.toDays() + " days " + hours + " hours and " + minutes + " minutes.");
+                System.out.println("Market will be open in " + duration.toDays() + " days " + hours + " hours and "
+                        + minutes + " minutes.");
                 try {
                     Thread.sleep(duration.toMillis());
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                 }
+            }
+            // for now just sleep for hour
+            int second = 1000;
+            // int hour = second * 3600;
+            try {
+                Thread.sleep(second * 5);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
             }
         }
     }
