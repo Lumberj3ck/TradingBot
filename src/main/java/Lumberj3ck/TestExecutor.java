028fc28a8bb28b5ccbe8f9391133979b3928825c
unknown <alanuldashev@gmail.com>
2024-12-16 00:04:03 +0100

    logging for user data provider implemented

--- src/main/java/Lumberj3ck/TestExecutor.java
+++ src/main/java/Lumberj3ck/TestExecutor.java
@@ -2,29 +2,31 @@ package Lumberj3ck;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class TestExecutor extends TradeExecutor {
     private Map<String, Boolean> openPositions = new HashMap<>();
+    private static final Logger logger = LogManager.getRootLogger();
 
     @Override
     public void buy(String symbol, String amount) {
         // super.buy(symbol, amount);
-        System.out.println(String.format("Heyyy you just bought %s of amount %s",
-                symbol, amount));
+        logger.info("Buying {} shares of {}", amount, symbol);
         openPositions.put(symbol, true);
     }
 
     @Override
     public void sell(String symbol, String amount) {
         // super.sell(symbol, amount);
-        System.out.println(String.format("Heyyy you just sold %s of amount %s",
-                symbol, amount));
+        logger.info("Selling {} shares of {}", amount, symbol);
         openPositions.put(symbol, false);
     }
 
     @Override
     protected boolean isEnoughFunds(Double stockPrice) {
         // TODO Auto-generated method stub
+        logger.error("isEnoughFunds method not implemented");
         throw new UnsupportedOperationException("Unimplemented method 'isEnoughFunds'");
     }
 
@@ -32,8 +34,11 @@ public class TestExecutor extends TradeExecutor {
     public boolean isPositionOpen(String symbol) {
         // for test
         if (openPositions.containsKey(symbol)) {
-            return openPositions.get(symbol);
+            boolean isOpen = openPositions.get(symbol);
+            logger.debug("Position for {} is {}", symbol, isOpen ? "open" : "closed");
+            return isOpen;
         } else {
+            logger.debug("No position found for {}", symbol);
             return false;
         }
     }
--- src/main/java/Lumberj3ck/UserDataProvider.java
+++ src/main/java/Lumberj3ck/UserDataProvider.java
@@ -3,6 +3,8 @@ package Lumberj3ck;
 import java.io.IOException;
 
 import org.json.JSONObject;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import okhttp3.Request;
 import okhttp3.Response;
@@ -10,11 +12,12 @@ import okhttp3.Response;
 public class UserDataProvider {
     private AlpacaKeysManager manager;
     private final String url;
+    private static final Logger logger = LogManager.getRootLogger();
 
     UserDataProvider() {
         this.manager = new AlpacaKeysManager();
-
         this.url = "https://paper-api.alpaca.markets/v2/account";
+        logger.debug("UserDataProvider initialized with URL: {}", url);
     }
 
     private JSONObject getUserInfo() {
@@ -24,29 +27,32 @@ public class UserDataProvider {
                 .build();
 
         try {
+            logger.debug("Sending request to get user info");
             Response response = this.manager.client.newCall(request).execute();
             if (response.isSuccessful() && response.body() != null) {
-                // Line below prints out whole information about the user like a BIG ASS line
-                // System.out.println(response.body().string());
-
+                logger.debug("Successfully received user info response");
                 return new JSONObject(response.body().string());
             } else {
-                System.out.println("HTTP Error: " + response.code() + " - " + response.message());
+                logger.error("HTTP Error: {} - {}", response.code(), response.message());
             }
         } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
+            logger.error("Error getting user info: {}", e.getMessage());
         }
         return null;
     }
 
     public Double getCash() {
         JSONObject responseBody = this.getUserInfo();
-        return Double.parseDouble(responseBody.getString("cash"));
+        Double cash = Double.parseDouble(responseBody.getString("cash"));
+        logger.info("Current cash balance: {}", cash);
+        return cash;
     }
 
     public Double getNonMarginableBuyingPower() {
         JSONObject responseBody = this.getUserInfo();
-        return Double.parseDouble(responseBody.getString("non_marginable_buying_power"));
+        Double power = Double.parseDouble(responseBody.getString("non_marginable_buying_power"));
+        logger.info("Current non-marginable buying power: {}", power);
+        return power;
     }
 
     public void getBalance() {
@@ -54,13 +60,10 @@ public class UserDataProvider {
         String cash = responseBody.getString("cash");
         String nonMarginableBuyingPower = responseBody.getString("non_marginable_buying_power");
         String equity = responseBody.getString("equity");
-        System.out.println(
-                "User's current non marginable buying power: " + responseBody.get("currency")
-                        + " "
-                        + nonMarginableBuyingPower);
-        System.out.println("User's current balance: " + responseBody.get("currency")
-                + " " + cash);
-        System.out.println("User's current equity: " + responseBody.get("currency") +
-                " " + equity);
+        String currency = responseBody.getString("currency");
+        
+        logger.info("User's current non marginable buying power: {} {}", currency, nonMarginableBuyingPower);
+        logger.info("User's current balance: {} {}", currency, cash);
+        logger.info("User's current equity: {} {}", currency, equity);
     }
 }
